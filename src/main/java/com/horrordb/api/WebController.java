daa0160e4b02b870e7097a789eecaf6e5a2ba56f
CorentinPineault <p-corentin@outlook.fr>
2021-06-27 12:27:56 +0200

    WIP Controller et Repository pour WebElement

--- src/main/java/com/horrordb/api/WebController.java
+++ src/main/java/com/horrordb/api/WebController.java
@@ -1,7 +1,8 @@
 package com.horrordb.api;
 
 import com.horrordb.entity.WebElement;
-//import com.horrordb.entity.WebSeries;
+import com.horrordb.entity.WebSeries;
+import com.horrordb.jpa.SeriesRepository;
 import com.horrordb.jpa.WebRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,10 +21,13 @@ public class WebController {
     @Autowired
     WebRepository webRepository;
 
+    @Autowired
+    SeriesRepository seriesRepository;
+
     //public WebController(WebRepository wsRepository){ this.webRepository = wsRepository; }
 
     @GetMapping("/weblist")
-    public ResponseEntity<List<WebElement>> getWebSeries(@RequestParam(required = false) String title) {
+    public ResponseEntity<List<WebElement>> getWebElements(@RequestParam(required = false) String title) {
         try {
             List<WebElement> wb = new ArrayList<>();
 
@@ -42,17 +46,53 @@ public class WebController {
         }
     }
 
+    @GetMapping("/serieslist")
+    public ResponseEntity<List<WebSeries>> getWebSeries(@RequestParam(required = false) String title) {
+        try {
+            List<WebSeries> ws = new ArrayList<>();
+
+            if (title == null)
+                seriesRepository.findAll().forEach(ws::add);
+            else
+                seriesRepository.findByNameContaining(title).forEach(ws::add);
+
+            if (ws.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(ws, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @PostMapping("/weblist")
-    void addWebSeries(@RequestBody WebElement we) {
+    void addWebElement(@RequestBody WebElement we) {
         webRepository.save(we);
     }
 
+    @PostMapping("/serieslist")
+    void addWebSeries(@RequestBody WebSeries ws) {
+        seriesRepository.save(ws);
+    }
+
     @GetMapping("/weblist/{id}")
-    public ResponseEntity<WebElement> getWebSeriesById(@PathVariable("id") long id) {
+    public ResponseEntity<WebElement> getWebElementsById(@PathVariable("id") long id) {
         Optional<WebElement> weData = webRepository.findById(id);
 
         if (weData.isPresent()) {
             return new ResponseEntity<>(weData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/serieslist/{id}")
+    public ResponseEntity<WebSeries> getWebSeriesById(@PathVariable("id") long id) {
+        Optional<WebSeries> wsData = seriesRepository.findById(id);
+
+        if (wsData.isPresent()) {
+            return new ResponseEntity<>(wsData.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
